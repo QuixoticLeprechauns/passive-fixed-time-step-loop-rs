ecfb7e67fd8fc0c45405c4cfdd664be49056bdf0
QuixoticLeprechauns <128657358+QuixoticLeprechauns@users.noreply.github.com>
2023-04-05 20:59:09 -0400

    Update pfts_loop.rs
    Updated to better adhere to the way rustations actually write and expect to use their code.

--- pfts_loop.rs
+++ pfts_loop.rs
@@ -1,17 +1,17 @@
 use std::thread;
 use std::time::Duration;
 
-pub fn pfts_loop(process: fn() -> i32, max_sleep_time: u64, min_sleep_time: u64)
-{
-    let mut retval: i32;
-    loop
-    {
-        let sleep_thread = thread::spawn(move || { thread::sleep(Duration::from_millis(max_sleep_time)); });
-        retval = process();
+pub fn pfts_loop(process: fn() -> Option<()>, max_sleep_time: u64, min_sleep_time: u64) {
+    loop {
+        let sleep_thread = thread::spawn(move || {
+            thread::sleep(Duration::from_millis(max_sleep_time));
+        });
+
+        let retval = process();
         thread::sleep(Duration::from_millis(min_sleep_time));
         sleep_thread.join().unwrap();
-        if retval == 0
-        {
+
+        if let None = retval {
             break;
         }
     }
